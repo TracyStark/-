1f143669c810a4d0dbeb59bf4351105013a32b28
TracyStark <zhouxy0814@icloud.com>
2024-12-18 16:53:00 +0800

    尝试测试

--- inference.py
+++ inference.py
@@ -1,67 +1,52 @@
-import time
+import torch
+import numpy as np
+import json
+from model.model import ResNetEncoder, DecoderWithAttention
 from config import *
-import torch.backends.cudnn as cudnn
-import torch.optim
-import torch.utils.data
-from torch import nn
-from tqdm import tqdm
-from torch.nn.utils.rnn import pack_padded_sequence
-from model.utils import *
-from model import metrics, dataloader, model
-from torch.utils.checkpoint import checkpoint as train_ck
+import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
 from model.dataloader import MyDataset
-import matplotlib.pyplot as plt
 
-test_path = './data/MyDataset_test/'
+# 加载模型
+encoder = ResNetEncoder()
+decoder = DecoderWithAttention(attention_dim=512, embed_dim=512, decoder_dim=512, vocab_size=414, dropout=0.5)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# 加载训练好的权重
+checkpoint = torch.load('BEST_checkpoint_MyDataset.pth')
+encoder.load_state_dict(checkpoint['encoder'])
+decoder.load_state_dict(checkpoint['decoder'])
 
-model.device = device
-
-if checkpoint is None:
-    print("no checkpoint found")
-    exit(1)
-else:
-    print(checkpoint)
-    checkpoint = torch.load(checkpoint)
-    start_epoch = checkpoint['epoch'] + 1
-    epochs_since_improvement = checkpoint['epochs_since_improvement']
-    best_score = checkpoint['score']
-    decoder = checkpoint['decoder']
-    encoder_optimizer = checkpoint['encoder_optimizer']
-    decoder_optimizer = checkpoint['decoder_optimizer']
-    encoder = checkpoint['encoder']
-
-decoder = decoder.to(device)
-encoder = encoder.to(device)
-
-test_dataset = MyDataset(test_path, is_train=False)
-
-test_loader = DataLoader(test_dataset, batch_size=test_batch_size, collate_fn=collate_fn_MyDataset, num_workers=0)
-
+# 将模型设置为评估模式
+encoder.eval()
 decoder.eval()
 
-if encoder is not None:
-    encoder.eval()
+# 加载测试数据集
+test_path = './data/'
+test_dataset = MyDataset(test_path, is_train=False)
+test_loader = DataLoader(test_dataset, batch_size=1, collate_fn=collate_fn_MyDataset, num_workers=0)
 
-words = []
-with open(vocab_path) as f:
-    words = f.read().splitlines()
-words.append("<start>")
-words.append("<end>")
+# 加载词汇表
+with open('word_map.json', 'r') as j:
+    word_map = json.load(j)
+reverse_word_map = {v: k for k, v in word_map.items()}
 
-with tqdm(enumerate(test_loader), leave=False, total=len(test_loader), position=0) as it:
-    for i, (imgs, caps, caplens) in it:
-        print(imgs.shape)
+# 进行推理
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+encoder = encoder.to(device)
+decoder = decoder.to(device)
+
+with torch.no_grad():
+    for imgs, caps, caplens in test_loader:
         imgs = imgs.to(device)
-        if encoder is not None:
-            imgs = encoder(imgs)
-            predictions, alphas, the_list = decoder.inference(imgs)
-            word_list = []
-            for i in the_list:
-                word_list.append(words[i])
-            with open("output_2.txt", "a") as file:
-                file.write(" ".join(word_list))  # 使用空格分隔列表元素
-                file.write("\n")
+        encoder_out = encoder(imgs)
+        h, c = decoder.init_hidden_state(encoder_out)
+        predictions, alphas, inference_list = decoder.inference(encoder_out)
+
+        # 将索引转换为单词
+        predicted_words = [reverse_word_map[idx] for idx in inference_list[0]]
+        print('Predicted words:', predicted_words)
+
+        # 保存输出标签
+        with open("output_labels.txt", "a") as file:
+            file.write(" ".join(predicted_words))  # 使用空格分隔列表元素
+            file.write("\n")
